861dcd17cbd137c9cf05800d5a347ec5135f5d4e
ahmed elsayed <ahmedabomoug@gmail.com>
2024-04-10 03:34:26 +0200

    CleanUp

--- server/server.ts
+++ server/server.ts
@@ -2,16 +2,14 @@ import express, { ErrorRequestHandler, RequestHandler } from "express";
 import { createPostHandlers, listPostHandlers } from "./handlers/postHandler";
 import asyncHandler from "express-async-handler";
 import { initDb } from "./datastore";
-import { SignInHandler, signUpHandler } from "./handlers/userHandler";
+import { SignInHandler, signUpHandler } from "./handlers/authHandler";
+import { requestloggrMiddlware } from "./middleware/loggerMiddleware";
+import { errHandler } from "./middleware/errHandlerMiddleware";
 
 (async () => {
   await initDb();
   const app = express();
 
-  const requestloggrMiddlware: RequestHandler = (req, res, next) => {
-    console.log(req.method, req.path, `body : `, req.body);
-    next();
-  };
   app.use(express.json());
   app.use(requestloggrMiddlware);
 
@@ -19,12 +17,7 @@ import { SignInHandler, signUpHandler } from "./handlers/userHandler";
   app.post("/v1/posts", asyncHandler(createPostHandlers));
   app.post("/v1/signup", asyncHandler(signUpHandler));
   app.post("/v1/signin", asyncHandler(SignInHandler));
-  const errHandler: ErrorRequestHandler = (err, req, res, next) => {
-    console.error("Uncaught exception:", err);
-    return res
-      .status(500)
-      .send("Oops, an unexpected error occurred, please try again");
-  };
+
   app.use(errHandler);
   app.listen(6000);
 })();
